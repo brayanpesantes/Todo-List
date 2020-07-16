c40fe4d389e7dafa70d0d3bc65794ffbdb745fc9
Alexis <brayananp.95@gmail.com>
2020-07-15 21:13:27 -0500

    Updates

---
+++ precache-manifest.70e0c26725c466eb98e599e2a95505a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05a6aec4464de701d1c03c0ddf621449",
+    "url": "/brayanpesantes/Todo-List/index.html"
+  },
+  {
+    "revision": "de1658ca5167ea41ac97",
+    "url": "/brayanpesantes/Todo-List/static/css/main.06e2f02f.chunk.css"
+  },
+  {
+    "revision": "21a573e9d241dfb7b5e4",
+    "url": "/brayanpesantes/Todo-List/static/js/2.4a64f5ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/brayanpesantes/Todo-List/static/js/2.4a64f5ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de1658ca5167ea41ac97",
+    "url": "/brayanpesantes/Todo-List/static/js/main.91a0134f.chunk.js"
+  },
+  {
+    "revision": "275e1d69e312bf48a13f",
+    "url": "/brayanpesantes/Todo-List/static/js/runtime-main.eccce333.js"
+  }
+]);
